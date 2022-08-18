f4302ff5b11ec90420a891427d49ccc374736f9b
max <me@maxlibin.com>
2022-08-19 01:17:38 +0800

    Wrap material tailwind, add header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
 import '../styles/globals.css'
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-tailwind/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+  return (
+    <>
+      <Navbar className="mt-6 mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
+        <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+          <Typography
+            as="a"
+            href="/"
+            variant="small"
+            className="mr-4 cursor-pointer py-1.5 font-bold"
+          >
+            <span>BlockChamp</span>
+          </Typography>
+          <div className="flex items-center space-x-2">
+            <Button color="green" variant="gradient" size="sm" className="lg:inline-block">
+              <span>Create</span>
+            </Button>
+            <Button variant="gradient" size="sm" className="lg:inline-block">
+              <span>Auth</span>
+            </Button>
+          </div>
+        </div>
+      </Navbar>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: ["./pages/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+})
