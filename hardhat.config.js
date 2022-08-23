897f19132c58d3af44a5ca448be40e6b1bd2b1ef
Ashton <54927531+hahaashton1@users.noreply.github.com>
2022-08-23 22:39:52 +0800

    Editted hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -25,7 +25,7 @@ module.exports = {
   mumbai: {
     url: "https://rpc-mumbai.maticvigil.com",
     chainId: 80001,
-    accounts: process.env.PRIVATE_KEY,
+    accounts: [process.env.PRIVATE_KEY],
     saveDeployments: true,
   },
   },etherscan: {
